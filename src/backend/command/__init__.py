ef963b1e05b8d886d25d5d76f4ce097d98f7c637
Tigor Mangatur Manurung <tigormanurung@Tigors-MacBook-Pro.local>
2016-04-17 23:37:31 +0700

    sensor

--- src/backend/command/__init__.py
+++ src/backend/command/__init__.py
@@ -2,6 +2,7 @@
 
 from SwitchHandler import SwitchHandler
 from CameraHandler import CameraHandler
+from SensorHandler import SensorHandler
 from CommandBase import CommandBase
 from CommandHandler import CommandHandler as Handler
 from GPIOHandler import GPIOHandler
