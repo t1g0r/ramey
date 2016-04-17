489b3c3741f4012638a46294a822b55f1ffe999a
System Administrator <root@Tigors-MacBook-Pro.local>
2016-04-17 12:11:17 +0700

    modify script

--- git.sh
+++ git.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+echo "Commit & Push To GIT"
+echo "========================"
 
+USERNAME=""
+PASSWORD=""
+MESSAGE=""
+
+#read -p "message :" msg
+
+
+
+
+
+echo $hy
 while [[ $# > 1 ]]
 do
 key="$1"
@@ -21,10 +34,27 @@ esac
 shift # past argument or value
 done
 
-# echo "${USERNAME}"
-# echo "${PASSWORD}"
-# echo "${MESSAGE}"
+if [ "$USERNAME" = "" ]
+then
+    read -p "USERNAME :" USERNAME
+fi
 
+if [ "$PASSWORD" = "" ]
+then
+    read -s -p "PASSWORD :" PASSWORD
+    printf '\n'
+fi
+
+if [ "$MESSAGE" = "" ]
+then
+    read -p "MESSAGE :" MESSAGE
+fi
+
+# $msg = {$MESSAGE}
+
+echo "Commiting to git.."
 sudo git add -A
 sudo git commit -m "${MESSAGE}"
+echo "Push.."
 sudo git push https://${USERNAME}:${PASSWORD}@github.com/t1g0r/ramey.git --all
+echo "Done."
